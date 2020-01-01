a2004c810d798ec60ed87324a645897dd05640e9
O.K <okgit@protonmail.com>
2020-01-01 16:17:28 +0100

    Initial commit

---
+++ update.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+cp $HOME/.Xresources .
+cp -r $XDG_CONFIG_HOME/bspwm/ .
+cp -r $XDG_CONFIG_HOME/compton/ .
+cp -r $XDG_CONFIG_HOME/dunst/ .
+cp -r $XDG_CONFIG_HOME/polybar/ .
+cp $XDG_CONFIG_HOME/redshift.conf .
+cp -r $XDG_CONFIG_HOME/rofi/ .
+cp -r $XDG_CONFIG_HOME/sxhkd/ .
+cp -r $XDG_CONFIG_HOME/youtube-dl/ .
